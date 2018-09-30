1f755c376fc64bac5fb879ecf86a8369f96ca0ac
Ira <irina.kalmykova83@gmail.com>
2018-09-30 12:56:49 +0300

    finished

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('style', function() {
 });
 
 gulp.task('script', function() {
-    return gulp.src('script/script.js')
+    return gulp.src('script/base.js, script.js')
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest('js/'))
